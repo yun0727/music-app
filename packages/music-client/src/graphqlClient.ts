5e122dc61499a896fb574966153ad847138afeed
yun <austru00@gmail.com>
2025-06-26 16:28:28 +0900

    fix: vercel로 배포

--- packages/music-client/src/graphqlClient.ts
+++ packages/music-client/src/graphqlClient.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-const endpoint = import.meta.env.VITE_API_HOST;
+const endpoint = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 export const graphqlClient = new GraphQLClient(endpoint);
